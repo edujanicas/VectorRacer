602e1474cb3f4f27e46da779811bd87d2ead7fa2
Eduardo Janicas <edujanicas@icloud.com>
2016-10-16 17:49:33 +0100

    Mooshack passed

--- scripts/SolF1.lisp
+++ scripts/SolF1.lisp
@@ -1,21 +1,22 @@
+;;; 78974 Eduardo Janicas 82448 Diana Antunes Grupo al113
 
 ;;; These functions, and any other ones needed must be implemented
 
 ;;; Utilizar estes includes para os testes na versao local
 ;;; comentar antes de submeter
-(load "datastructures.lisp")
-(load "auxfuncs.lisp")
+;(load "datastructures.lisp")
+;(load "auxfuncs.lisp")
 
 ;;; Utilizar estes includes para a versao a submeter
 ; tirar o comentario antes de submeter
-;(load "datastructures.fas")
-;(load "auxfuncs.fas")
+(load "datastructures.fas")
+(load "auxfuncs.fas")
 
 (defun isObstaclep (pos track)
   "check if there is an obstacle at position pos of the track"
   (if (car (nthcdr (cadr pos)
   		(car (nthcdr (car pos)
-			(track-env *track*)))))
+			(track-env track)))))
   nil t))
 
 
@@ -29,16 +30,18 @@
 
 (defun nextState (st act)
   "generate the nextState after state st and action act"
-  	(make-STATE
-	:POS ( list (+ (car (state-pos st)) (car (state-vel st)) (car act))
-		  (+ (cadr (state-pos st)) (cadr (state-vel st)) (cadr act)))
-	:VEL ( list (+ (car (state-vel st)) (car act))
-		  (+ (cadr (state-vel st)) (cadr act)))
-	:ACTION act
-	:TRACK (state-track st)
-	:OTHER (state-other st)
-	:COST (cond
-				((isObstaclep :POS (state-track st)) 20)
-				((isGoalp st) -100)
-				(T 1))
+  	(defparameter midState (make-STATE
+		:POS ( list (+ (car (state-pos st)) (car (state-vel st)) (car act))
+			  (+ (cadr (state-pos st)) (cadr (state-vel st)) (cadr act)))
+		:VEL ( list (+ (car (state-vel st)) (car act))
+			  (+ (cadr (state-vel st)) (cadr act)))
+		:ACTION act
+		:TRACK (state-track st)
+		:OTHER (state-other st)
 	))
+	(setf (state-cost midState) (cond
+				((isObstaclep (state-pos midState) (state-track st)) 20)
+				((isGoalp midState) -100)
+				(T 1)))
+	midState
+)
